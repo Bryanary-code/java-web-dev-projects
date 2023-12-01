5a65571adf8fd2d9742adf4c57e37e1be2dae8dd
Bryan Wulfert <bwulfert@gmail.com>
2023-11-30 21:39:30 -0600

    added while loop

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -5,8 +5,12 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter circle radius: ");
-
         double radius = input.nextDouble();
+
+        while(radius < 0 ) {
+            System.out.println("Please enter a valid number.");
+                  radius = input.nextDouble();
+        }
         System.out.println("A circle with a radius of " + radius + " is " + Circle.getArea(radius));
     }
 }
