ef3d4812f05b4c97c623ee6471a2a5155e5abf94
Bryan Wulfert <bwulfert@gmail.com>
2023-12-08 20:06:38 -0600

    classes part one exercise

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,7 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student bryan = new Student("Bryan", 91, 1, 4.0);
+
     }
 }
