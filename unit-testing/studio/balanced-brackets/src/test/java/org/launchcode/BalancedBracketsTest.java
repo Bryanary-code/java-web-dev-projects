2ee8ef37058eb0e0028b1cf9e2a98a909b30202a
Bryan Wulfert <bwulfert@gmail.com>
2023-12-18 15:28:39 -0600

    unit testing studio test

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testBalancedWithOtherChars() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void testUnbalancedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testUnbalancedWithOtherChars() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("L]aunchCode["));
+    }
+
+    @Test
+    public void testOnlyOneLeftBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testOnlyOneRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void testOneLeftBracketWithOtherChars() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch["));
+    }
+
+    @Test
+    public void testOneRightBracketWithOtherChars() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]"));
     }
 }
